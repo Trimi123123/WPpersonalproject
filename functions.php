140dad221dc02b231876e4177dc10657464d4c8a
Trimi123123 <trimmerovci10@gmail.com>
2026-01-12 19:19:13 +0100

    d

--- functions.php
+++ functions.php
@@ -1,25 +1,24 @@
 <?php
-// Enqueue theme assets
+
 function protheme_assets() {
   wp_enqueue_style('protheme-style', get_stylesheet_uri());
   wp_enqueue_script('protheme-js', get_template_directory_uri() . '/assets/js/main.js', [], '1.0.0', true);
 }
 add_action('wp_enqueue_scripts', 'protheme_assets');
 
-// Theme setup
+
 function protheme_setup() {
-    // Add theme supports
+
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
     add_theme_support('html5', ['search-form', 'gallery']);
     add_theme_support('custom-logo');
 
-    // Register menus
+
     register_nav_menus([
         'main-menu' => 'Main Menu',
     ]);
 
-    // Disable admin bar on frontend
     show_admin_bar(false);
 }
 add_action('after_setup_theme', 'protheme_setup');
